1d58985b0d1aed5bbc07365e9241f441c80e2200
benjamin verdès <verdesb.devacc@gmail.com>
2023-10-22 22:39:09 +0200

    add sign Up button

--- FRONT/src/components/Header/Header.jsx
+++ FRONT/src/components/Header/Header.jsx
@@ -8,11 +8,12 @@ const Header = () => {
                 <nav className="flex flex-row items-center h-full ">
                     <div className="w-3/6 pl-1">
                         <Link className="flex flex-row items-center">
-                            <img className="w-7 h-7" src={logo} alt="logo"></img>
+                            <img className="w-8 h-8" src={logo} alt="logo"></img>
                             <span className="text-blue-500 text-lg">eather-Caster</span>
                         </Link>
                     </div>
-                    <div className="w-3/6 flex flex-row justify-end pr-1">
+                    <div className="w-3/6 flex flex-row justify-end pr-1 gap-2">
+                        <button className="bg-white hover:bg-blue-500 text-blue-500 hover:text-white font-bold py-2 px-4 border border-blue-700 rounded">S'inscrire</button>
                         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Se connecter</button>
                     </div>
                 </nav>
