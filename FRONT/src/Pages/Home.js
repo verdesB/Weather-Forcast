bf211189655e7f9bac9952fd93ea2f13c5c67cc3
benjamin verdès <verdesb.devacc@gmail.com>
2023-10-22 19:57:43 +0200

    remove sidebar ===>  MainHome component

--- FRONT/src/Pages/Home.js
+++ FRONT/src/Pages/Home.js
@@ -1,15 +1,13 @@
 import React from "react"
 import Header from "../components/Header/Header"
-import Sidebar from "../components/Sidebar/Sidebar"
+import MainHome from "../components/MainHome/MainHome"
+
 
 const Home = () => {
     return(
         <>
         <Header/>
-        <main className="main flex flex-row flex-grow">
-        <Sidebar/>
-        <div className="flex-grow">content</div>
-        </main>
+        <MainHome/>
         </>
     )
 }
