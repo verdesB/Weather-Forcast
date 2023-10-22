cb48a7809f6cb6c3da9c0c4955caea96e7a983be
benjamin verdès <verdesb.devacc@gmail.com>
2023-10-22 20:14:05 +0200

    add hidden for the main content

--- FRONT/src/components/MainHome/MainHome.jsx
+++ FRONT/src/components/MainHome/MainHome.jsx
@@ -12,7 +12,7 @@ const MainHome = () => {
         <main className="main relative flex flex-row flex-grow">
             <Sidebar isOpen={isOpen} toggleOpen={toggleOpen}/>
             <div className={` flex-grow transition-all duration-200 ${isOpen ? 'ml-64' : 'ml-12' }`}>
-                <div className="p-12  h-full">content</div>
+                <div className="p-4  h-full">content</div>
                 </div>
         </main>
     )
